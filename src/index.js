fbb0d57f92ff009bf496c36bb0aa1b6042b0c28c
cyrus <cyrusliew02@gmail.com>
2020-10-18 20:26:52 +1300

    Laybuy Code test - dollar to words

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const converter = require('number-to-words');
+
+function youSayDollarISayWords(amount) {
+    const processedAmount = amount.toFixed(2);
+    const hasCents = processedAmount % 1 !== 0;
+    let string = '';
+
+    const dollar = processedAmount.toString().split('.')[0];
+
+    if (amount < 0) {
+        throw new Error('Stay positive is the key to success.');
+    }
+
+    if (amount > 1000) {
+        throw new Error('Let\'s hold on right there! I can only say a number that is up to a thousand (1000).');
+    }
+
+    if (dollar >= 0) {
+        string += converter.toWords(dollar);
+        string += ' dollars'
+    }
+
+    if (hasCents) {
+        const cents = processedAmount.toString().split('.')[1];
+
+        if (dollar > 0) {
+            string += ' and ';    
+        }
+
+        if (dollar === '0') {
+            string = '';
+        }
+
+        string +=  converter.toWords(cents);
+        string += ' cents';
+    }
+    
+    // console.log(string.replace('-', ' '));
+    return string.replace(/-/g, ' ');
+}
+
+module.exports = {
+    youSayDollarISayWords,
+}
---
+++ tests/dollars.test.js
@@ -0,0 +1,57 @@
+const { youSayDollarISayWords } = require('../src/index');
+
+test('Cent', () => {
+    expect(youSayDollarISayWords(0.03)).toBe('three cents');
+});
+
+test('two digits cents below 0.2', () => {
+    expect(youSayDollarISayWords(0.13)).toBe('thirteen cents');
+});
+
+test('Cent with 3 decimal places', () => {
+    expect(youSayDollarISayWords(0.057)).toBe('six cents');
+})
+
+test('Single digit', () => {
+    expect(youSayDollarISayWords(7)).toBe('seven dollars');
+})
+
+test('Zero dollars', () => {
+    expect(youSayDollarISayWords(0)).toBe('zero dollars');
+})
+
+test('Two digits', () => {
+    expect(youSayDollarISayWords(11)).toBe('eleven dollars');
+})
+
+test('Two digits with two digit cents below 20', () => {
+    expect(youSayDollarISayWords(11.83)).toBe('eleven dollars and eighty three cents');
+})
+
+test('Two digits with two digits cents', () => {
+    expect(youSayDollarISayWords(12.14)).toBe('twelve dollars and fourteen cents');
+})
+
+test('Two digits above twenty', () => {
+    expect(youSayDollarISayWords(23)).toBe('twenty three dollars');
+})
+
+test('Three digits', () => {
+    expect(youSayDollarISayWords(123)).toBe('one hundred twenty three dollars');
+})
+
+test('Three digits with cents', () => {
+    expect(youSayDollarISayWords(871.43)).toBe('eight hundred seventy one dollars and forty three cents');
+})
+
+test('A thoursand', () => {
+    expect(youSayDollarISayWords(1000)).toBe('one thousand dollars');
+})
+
+test('Exceed 1000', () => {
+    expect(() => youSayDollarISayWords(1000.1)).toThrow('up to a thousand');
+})
+
+test('Negative number', () => {
+    expect(() => youSayDollarISayWords(-18.1)).toThrow('positive');
+})
